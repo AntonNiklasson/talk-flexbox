be984e931cb31761d309f176be061480c78cd742
Anton Niklasson <niklasson.anton@gmail.com>
2015-02-06 10:42:19 +0100

    First commit.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+	return gulp.src('.')
+		.pipe(webserver({
+			livereload: true,
+			directoryListing: false,
+			open: false,
+            host: "0.0.0.0"
+		}));
+});
+
+gulp.task('default', ['webserver'], function() {
+	gulp.watch(['css/stylus/**/*.styl'], ['css']);
+});
